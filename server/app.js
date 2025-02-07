a3f02fe3b967d0a5c70dff9e22dd86d0c8bd9206
soul-SUMAN <sumanmondal1009@gmail.com>
2025-02-07 17:25:35 +0530

    Fixed CORS, static files, and video serving

--- server/app.js
+++ server/app.js
@@ -14,6 +14,12 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
 
 app.use(express.static(path.join(__dirname, '../public')));
 
